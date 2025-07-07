1eb7eec6f50cd8d16b8664d12425837a4e865485
BPk <unrealkluzz123@gmail.com>
2025-07-07 23:29:04 +0100

    Parse Numbers

--- NumbersDates/ParseNumbers/obj/Debug/net6.0/ParseNumbers.AssemblyInfo.cs
+++ NumbersDates/ParseNumbers/obj/Debug/net6.0/ParseNumbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParseNumbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbebc92a91925e1aa5ce6970a08efb07ae9f57b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4a86c9913904f952a4313c6e622224c8cc53ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParseNumbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParseNumbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
